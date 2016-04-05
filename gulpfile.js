bf79dda85e21d025bdda8afe198bd7d81a687030
Brandon Beasley <beasleyb@msu.edu>
2016-04-05 17:01:33 -0400

    Setting up file structure and gulp plugins.

--- gulpfile.js
+++ gulpfile.js
@@ -2,33 +2,54 @@
 // Required
 // ------------------------------------
 
-var gulp 	= require('gulp');
-var sass 	= require('gulp-sass');
-var uglify 	= require('gulp-uglify');
+var browserSync 	= require('browser-sync').create();
+var merge 			= require('merge-stream');
+var gulp 			= require('gulp');
+var sass 			= require('gulp-sass');
+var uglify 			= require('gulp-uglify');
+var autoprefixer	= require('gulp-autoprefixer');
 
 // ------------------------------------
 // Tasks
 // ------------------------------------
 
-// Compile custom scss
+// Compile, minify, and prefix custom scss
 gulp.task('sass:custom', function () {
-	return gulp.src('src/_scss/*.scss')
-		.pipe(sass().on('error', sass.logError))
-    	.pipe(gulp.dest('src/css'));
+	return gulp
+		.src('src/scss/*.scss')
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(autoprefixer({ browsers: ['last 2 versions'], cascade: false }))
+    	.pipe(gulp.dest('public/css'));
 });
 
-// Compile vendor scss
+// Compile, minify, and prefix vendor scss
 gulp.task('sass:vendor', function () {
-	return gulp.src('src/_scss/vendor/*.scss')
-		.pipe(sass().on('error', sass.logError))
-    	.pipe(gulp.dest('src/css/vendor'));
+	return gulp
+		.src('src/scss/vendor/*.scss')
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(autoprefixer({ browsers: ['last 2 versions'], cascade: false }))
+    	.pipe(gulp.dest('public/css/vendor'));
+});
+
+// Copy files to public
+gulp.task('copy', function() {
+	var html = gulp
+		.src('src/index.html')
+		.pipe(gulp.dest('public'));
+
+	var images = gulp
+		.src('src/img/*.*')
+		.pipe(gulp.dest('public/img'));
+
+	return merge(html, images);
 });
 
 // Watch files
 gulp.task('watch', function () {
-	gulp.watch('src/_scss/*.scss', ['sass:custom']);
-	gulp.watch('src/_scss/vendor/*.scss', ['sass:vendor']);
+	gulp.watch('src/scss/*.scss', ['sass:custom']);
+	gulp.watch('src/scss/vendor/*.scss', ['sass:vendor']);
+	gulp.watch('src/index.html', ['copy']);
 });
 
-// Default task
-gulp.task('default', ['sass:custom', 'sass:vendor', 'watch']);
+// Default
+gulp.task('default', ['sass:custom', 'sass:vendor', 'copy', 'watch']);
